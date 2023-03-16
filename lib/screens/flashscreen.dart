db78adb42e22edd719b71c9e8e96147d3663b02f
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-03-16 23:03:56 +0100

    Burger App

--- lib/screens/flashscreen.dart
+++ lib/screens/flashscreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FlashScreen extends StatefulWidget {
@@ -20,9 +21,18 @@ class _FlashScreenState extends State<FlashScreen> {
         child: Center(child: Column(
           mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            Container(height: 150,),
             Image.network("https://www.transparentpng.com/thumb/food/PIIPbV-cola-potato-chips-burger-food-free-png.png", width: 250,),
             Container(height: 50,),
-            const Text("Try the best burgers now !", textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 35),),
+            const Text("Try the best \nburgers now !", textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 45, fontWeight: FontWeight.bold, fontFamily: "JosefinSans"),),
+            Container(height: 35,),
+            const Text("Find different flavours and \nenjoy them at every moments", textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold, fontFamily: "JosefinSans"),),
+            Container(height: 45,),
+            Container(
+              padding: EdgeInsets.all(20), width: 70, height: 70, 
+              child: Icon(Icons.forward),
+              decoration: BoxDecoration( color: Colors.white24, borderRadius: BorderRadius.circular(35)),
+            )
           ],
         ),) 
       ),
