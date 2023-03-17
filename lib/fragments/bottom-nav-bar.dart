76a04c500ee0721f177aa348bf35ccc555e24a62
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-03-17 22:11:51 +0100

    Burger App

--- lib/fragments/bottom-nav-bar.dart
+++ lib/fragments/bottom-nav-bar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-// bottom navigation ba
+// bottom navigation bar
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({Key? key,}) : super(key: key);
   @override
