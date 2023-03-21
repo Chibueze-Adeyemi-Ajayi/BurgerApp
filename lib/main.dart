6ae896e1b1d5edf975d1ebd221ef312120dbedb3
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-03-21 19:35:41 +0100

    Burger App

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ callback (value) {
 
 class _MyHomePageState extends State<MyHomePage> {
   int index = 0;
-  // state
+  // state 
   void useState (value) {
     setState(() {
       index = value;
