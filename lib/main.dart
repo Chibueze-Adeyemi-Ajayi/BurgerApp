a1fd7ea750a4b2cb18138c86ee566408cf772b57
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-03-21 19:38:44 +0100

    Burger App

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
       index = value;
     });
   }
-  // app
+  // appl
   List <Widget> screens = const <Widget> [
      FlashScreen(callback: callback,), 
      Store(callback: callback),
