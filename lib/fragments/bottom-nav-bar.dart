b23d57eb128bb682a2070f48724bc37052faaca4
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-03-24 20:01:18 +0100

    Burger App

--- lib/fragments/bottom-nav-bar.dart
+++ lib/fragments/bottom-nav-bar.dart
@@ -7,7 +7,7 @@ class BottomNavBar extends StatefulWidget {
 }
 
 class _BottomNavBarState extends State<BottomNavBar> {
-  // nav bar icon arra
+  // nav bar icon array
   List <Icon> icons = [
     Icon(Icons.home, color: Colors.white60,),
     Icon(Icons.store, color: Colors.white60,),
