7eee6068609fb11a560184755ac740c052adbfce
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-03-24 19:54:47 +0100

    Burger App

--- lib/fragments/bottom-nav-bar.dart
+++ lib/fragments/bottom-nav-bar.dart
@@ -5,7 +5,7 @@ class BottomNavBar extends StatefulWidget {
   @override
   State<BottomNavBar> createState() => _BottomNavBarState();
 }
-// bottom navigati
+// bottom navigat
 class _BottomNavBarState extends State<BottomNavBar> {
   // nav bar icon
   List <Icon> icons = [
