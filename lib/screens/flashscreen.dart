fca96c4115eb9e5597c38b86977d6d20e15c5174
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-03-25 20:24:13 +0100

    Burger App

--- lib/screens/flashscreen.dart
+++ lib/screens/flashscreen.dart
@@ -12,7 +12,7 @@ class FlashScreen extends StatefulWidget {
 }
 
 class _FlashScreenState extends State<FlashScreen> {
-  // callb
+  // callba
 _FlashScreenState({required this.callback});
 final callback;
 final PageController _pageController = PageController();
