39422e243bdf07a67cf400d31723215e6f5f1eda
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-03-26 23:53:22 +0100

    Burger App

--- lib/fragments/grid-menu.dart
+++ lib/fragments/grid-menu.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-// list of subsidiary product in grid view widg
+// list of subsidiary product in grid view widge
 class GridMenu extends StatefulWidget {
   const GridMenu({Key? key,}) : super(key: key);
   @override
