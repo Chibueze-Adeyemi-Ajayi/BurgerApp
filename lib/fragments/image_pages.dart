6f5949a6c96c558681aa9b93974ac43259459a55
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-03-26 23:41:46 +0100

    Burger App

--- lib/fragments/image_pages.dart
+++ lib/fragments/image_pages.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-// image widget on p
+// image widget on pa
 class ImagePage extends StatefulWidget {
   const ImagePage({Key? key,}) : super(key: key);
   @override
